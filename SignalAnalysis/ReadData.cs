d1cb45010b111e84addab58a2d00907a8a3a7ba0
arthurits <simply_alfy@hotmail.com>
2021-12-05 11:13:05 +0100

    Refactor code

---
+++ SignalAnalysis/ReadData.cs
@@ -0,0 +1,197 @@
+﻿using System.Text;
+
+namespace SignalAnalysis
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Reads data from an elux file and stores it into _signalData.
+        /// </summary>
+        /// <param name="FileName">Path (including name) of the elux file</param>
+        private void ReadELuxData(string FileName)
+        {
+            using var fs = File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var sr = new StreamReader(fs, Encoding.UTF8);
+
+            string? strLine = sr.ReadLine();
+            if (strLine != null && strLine != "ErgoLux data")
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+
+            // Better implement a try parse block. Each read line should throw an exception instead of "return"
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Start time: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("End time: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Total measuring time: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Number of sensors: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !int.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nSeries)) return;
+            if (nSeries == 0) return;
+            nSeries += 6;
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Number of data points: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !int.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nPoints)) return;
+            if (nPoints == 0) return;
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Sampling frequency: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !double.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nSampleFreq)) return;
+
+            strLine = sr.ReadLine();    // Empty line
+
+            strLine = sr.ReadLine();    // Column header lines
+            _series = strLine != null ? strLine.Split('\t') : Array.Empty<string>();
+
+            InitializeDataArrays(sr);
+        }
+
+        /// <summary>
+        /// Readas data from a sig file and stores it into _signalData.
+        /// </summary>
+        /// <param name="FileName">Path (including name) of the elux file</param>
+        private void ReadSigData(string FileName)
+        {
+            using var fs = File.Open(FileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+            using var sr = new StreamReader(fs, Encoding.UTF8);
+
+            string? strLine = sr.ReadLine();
+            if (strLine != null && strLine != "SignalAnalysis data")
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Number of series: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !int.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nSeries)) return;
+            if (nSeries == 0) return;
+            nSeries += 6;
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Number of points: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !int.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nPoints)) return;
+            if (nPoints == 0) return;
+
+            strLine = sr.ReadLine();
+            if (strLine != null && !strLine.Contains("Sampling frequency: ", StringComparison.Ordinal))
+            {
+                using (new CenterWinDialog(this))
+                {
+                    MessageBox.Show("Unable to read data from file:\nwrong file format.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                return;
+            }
+            if (strLine == null || !double.TryParse(strLine[(strLine.IndexOf(":") + 1)..], out nSampleFreq)) return;
+
+            strLine = sr.ReadLine();    // Empty line
+
+            strLine = sr.ReadLine();    // Column header lines
+            _series = strLine != null ? strLine.Split('\t') : Array.Empty<string>();
+
+            InitializeDataArrays(sr);
+        }
+
+        /// <summary>
+        /// Reads the numeric data section pointed at.
+        /// </summary>
+        /// <param name="sr">This reader should be pointing to the beginning of the numeric data section</param>
+        private void InitializeDataArrays(StreamReader sr)
+        {
+            string? strLine;
+
+            // Initialize data arrays
+            _signalX = new double[nPoints];
+            _signalData = new double[nSeries][];
+            for (int i = 0; i < nSeries; i++)
+                _signalData[i] = new double[nPoints];
+
+            // Read data into _plotData
+            for (int i = 0; i < _signalData.Length; i++)
+            {
+                _signalData[i] = new double[nPoints];
+            }
+            string[] data;
+            int row = 0, col = 0;
+            while ((strLine = sr.ReadLine()) != null)
+            {
+                data = strLine.Split("\t");
+                for (row = 0; row < data.Length; row++)
+                {
+                    double.TryParse(data[row], out _signalData[row][col]);
+                }
+                col++;
+            }
+        }
+
+    }
+}
---
+++ SignalAnalysis/UpdatePlots.cs
@@ -0,0 +1,84 @@
+﻿namespace SignalAnalysis
+{
+    partial class FrmMain
+    {
+        private void UpdateOriginal()
+        {
+            plotOriginal.Plot.Clear();
+            plotOriginal.Plot.AddSignal(_signalData[cboSeries.SelectedIndex], nSampleFreq, label: cboSeries.SelectedItem.ToString());
+            plotOriginal.Plot.Title("Input signal");
+            plotOriginal.Plot.YLabel("Amplitude");
+            plotOriginal.Plot.XLabel("Time (seconds)");
+            plotOriginal.Plot.AxisAuto(0);
+            plotOriginal.Refresh();
+        }
+
+        private void UpdateKernel(FftSharp.IWindow window)
+        {
+            double[] kernel = window.Create(nPoints);
+            double[] pad = ScottPlot.DataGen.Zeros(kernel.Length / 4);
+            double[] ys = pad.Concat(kernel).Concat(pad).ToArray();
+
+            plotWindow.Plot.Clear();
+            plotWindow.Plot.AddSignal(ys, nSampleFreq, Color.Red);
+            plotWindow.Plot.AxisAuto(0);
+            plotWindow.Plot.Title($"{window} Window");
+            plotWindow.Plot.YLabel("Amplitude");
+            plotWindow.Plot.XLabel("Time (seconds)");
+            plotWindow.Refresh();
+        }
+
+        private void UpdateWindowed(double[] signal)
+        {
+            plotApplied.Plot.Clear();
+            plotApplied.Plot.AddSignal(signal, nSampleFreq);
+            plotApplied.Plot.Title("Windowed signal");
+            plotApplied.Plot.YLabel("Amplitude");
+            plotApplied.Plot.XLabel("Time (seconds)");
+            plotApplied.Plot.AxisAuto(0);
+            plotApplied.Refresh();
+        }
+
+        private void UpdateFractal(bool progressive = false)
+        {
+            if (_signalData.Length == 0) return;
+
+            var cursor = this.Cursor;
+            this.Cursor = Cursors.WaitCursor;
+
+            var fractalDim = new FractalDimension(nSampleFreq, _signalData[cboSeries.SelectedIndex], progressive);
+            var dimension = fractalDim.Dimension;
+
+            plotFractal.Plot.Clear();
+            if (progressive && fractalDim.ProgressDim != null)
+            {
+                plotFractal.Plot.AddSignal(fractalDim.ProgressDim, nSampleFreq, label: cboSeries.SelectedItem.ToString());
+            }
+            else
+            {
+                plotFractal.Plot.AddLine(0, dimension, (0, nPoints / nSampleFreq));
+            }
+            plotFractal.Plot.Title("Fractal dimension" + (progressive ? " (progressive)" : String.Empty) + " (H = " + dimension.ToString("#.00000") + ")");
+            plotFractal.Plot.YLabel("Dimension (H)");
+            plotFractal.Plot.XLabel("Time (seconds)");
+            plotFractal.Plot.AxisAuto(0);
+            plotFractal.Refresh();
+
+            this.Cursor = cursor;
+        }
+
+        private void UpdateFFT(double[] signal)
+        {
+            double[] ys = chkLog.Checked ? FftSharp.Transform.FFTpower(signal) : FftSharp.Transform.FFTmagnitude(signal);
+
+            // Plot the results
+            plotFFT.Plot.Clear();
+            plotFFT.Plot.AddSignal(ys, (double)ys.Length / nSampleFreq);
+            plotFFT.Plot.Title("Fast Fourier Transform");
+            plotFFT.Plot.YLabel(chkLog.Checked ? "Power (dB)" : "Magnitude (RMS²)");
+            plotFFT.Plot.XLabel("Frequency (Hz)");
+            plotFFT.Plot.AxisAuto(0);
+            plotFFT.Refresh();
+        }
+    }
+}
