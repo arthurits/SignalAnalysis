2c3bdae0f08e0c2c760e7e8c83b1b41b5d3853e6
—ins— <insht_cnct@hotmail.es>
2024-01-16 09:23:31 +0100

    Change default value

--- SignalAnalysis/clsSettings.cs
+++ SignalAnalysis/clsSettings.cs
@@ -14,7 +14,7 @@ public class AppSettings
     /// Remember window position on start up
     /// </summary>
     [JsonPropertyName("Window position")]
-    public bool WindowPosition { get; set; } = false;
+    public bool WindowPosition { get; set; } = true;
     [JsonPropertyName("Window top")]
     public int WindowTop { get; set; } = 0;
     [JsonPropertyName("Window left")]
