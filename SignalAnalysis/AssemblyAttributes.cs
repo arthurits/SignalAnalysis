12ab14933b3d92c672637b6f788eae22eba52172
arthurits <simply_alfy@hotmail.com>
2024-01-20 13:03:41 +0100

    Create static class to access assemby attributes

---
+++ SignalAnalysis/AssemblyAttributes.cs
@@ -0,0 +1,90 @@
+﻿using System.Reflection;
+
+namespace ErgoCalc;
+public static class AssemblyAttributes
+{
+    public static string AssemblyTitle
+    {
+        get
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+            if (attributes.Length > 0)
+            {
+                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                if (titleAttribute.Title != string.Empty)
+                {
+                    return titleAttribute.Title;
+                }
+            }
+            return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location);
+        }
+    }
+
+    public static string AssemblyVersion
+    {
+        get
+        {
+            return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "?";
+        }
+    }
+
+    public static string AssemblyDescription
+    {
+        get
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+            if (attributes.Length == 0)
+            {
+                return string.Empty;
+            }
+            return String.Concat(((AssemblyDescriptionAttribute)attributes[0]).Description,
+                ".",
+                System.Environment.NewLine,
+                System.Environment.NewLine,
+                "No commercial use allowed whatsoever. Contact the author for any inquires.",
+                System.Environment.NewLine,
+                System.Environment.NewLine,
+                "If you find this software useful, please consider supporting it!");
+        }
+    }
+
+    public static string AssemblyProduct
+    {
+        get
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+            if (attributes.Length == 0)
+            {
+                return string.Empty;
+            }
+            return ((AssemblyProductAttribute)attributes[0]).Product;
+        }
+    }
+
+    public static string AssemblyCopyright
+    {
+        get
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
+            if (attributes.Length == 0)
+            {
+                return string.Empty;
+            }
+            return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+        }
+    }
+
+    public static string AssemblyCompany
+    {
+        get
+        {
+            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
+            if (attributes.Length == 0)
+            {
+                return string.Empty;
+            }
+            return ((AssemblyCompanyAttribute)attributes[0]).Company;
+        }
+    }
+
+}
--- SignalAnalysis/FrmAbout.cs
+++ SignalAnalysis/FrmAbout.cs
@@ -1,107 +1,18 @@
-﻿using System.Reflection;
+﻿using ErgoCalc;
 
 namespace SignalAnalysis;
 
 partial class FrmAbout : Form
 {
-
-    #region Descriptores de acceso de atributos de ensamblado
-
-    public string AssemblyTitle
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-            if (attributes.Length > 0)
-            {
-                AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                if (titleAttribute.Title != "")
-                {
-                    return titleAttribute.Title;
-                }
-            }
-            return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().Location);
-        }
-    }
-
-    public string AssemblyVersion
-    {
-        get
-        {
-            return Assembly.GetExecutingAssembly().GetName().Version?.ToString() ?? "?";
-        }
-    }
-
-    public string AssemblyDescription
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return String.Concat(((AssemblyDescriptionAttribute)attributes[0]).Description,
-                ".",
-                System.Environment.NewLine,
-                System.Environment.NewLine,
-                "No commercial use allowed whatsoever. Contact the author for any inquires.",
-                System.Environment.NewLine,
-                System.Environment.NewLine,
-                "If you find this software useful, please consider supporting it!");
-        }
-    }
-
-    public string AssemblyProduct
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyProductAttribute)attributes[0]).Product;
-        }
-    }
-
-    public string AssemblyCopyright
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-        }
-    }
-
-    public string AssemblyCompany
-    {
-        get
-        {
-            object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-            if (attributes.Length == 0)
-            {
-                return "";
-            }
-            return ((AssemblyCompanyAttribute)attributes[0]).Company;
-        }
-    }
-
-    #endregion
-
     public FrmAbout()
     {
         InitializeComponent();
         // this.Text = String.Format("About {0}", AssemblyTitle);
-        this.labelProductName.Text = String.Format(StringResources.AboutProductName, AssemblyProduct);
-        this.labelVersion.Text = String.Format(StringResources.AboutVersion, AssemblyVersion);
-        this.labelCopyright.Text = String.Format(StringResources.AboutCopyright, AssemblyCopyright);
-        this.labelCompanyName.Text = String.Format(StringResources.AboutCompanyName, AssemblyCompany);
-        this.textBoxDescription.Text = String.Format(StringResources.AboutDescription, AssemblyDescription);
+        this.labelProductName.Text = String.Format(StringResources.AboutProductName, AssemblyAttributes.AssemblyProduct);
+        this.labelVersion.Text = String.Format(StringResources.AboutVersion, AssemblyAttributes.AssemblyVersion);
+        this.labelCopyright.Text = String.Format(StringResources.AboutCopyright, AssemblyAttributes.AssemblyCopyright);
+        this.labelCompanyName.Text = String.Format(StringResources.AboutCompanyName, AssemblyAttributes.AssemblyCompany);
+        this.textBoxDescription.Text = String.Format(StringResources.AboutDescription, AssemblyAttributes.AssemblyDescription);
 
         // Set form icons and images
         //if (System.IO.File.Exists(path + @"\images\about.ico")) this.Icon = new Icon(path + @"\images\about.ico");
