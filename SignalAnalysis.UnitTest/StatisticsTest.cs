b97e4916aee54e690407eb6391e22e08c625b5a3
—ins— <insht_cnct@hotmail.es>
2024-02-19 13:09:04 +0100

    Rename file

--- SignalAnalysis.UnitTest/StatisticsTest.cs
+++ SignalAnalysis.UnitTest/StatisticsTest.cs
@@ -1,7 +1,7 @@
 namespace SignalAnalysis.UnitTest;
 
 [TestClass]
-public class SignalAnalysisTest
+public class StatisticsTest
 {
     readonly double[] dataRandom = [0.635332247, 0.870181136, 0.678631034, 1.078938819, 1.105371046, 1.160009639, 1.091093042, 1.01799636, 0.992046413, 1.40562247, 0.74047614, 1.01297892,
             0.930566431, 0.829267703, 1.268018434, 1.2646188, 0.943980255, 0.53140922, 1.89863075, 0.806829855, 0.428950107, 2.020562594, 0.787877134, 1.195852193, 1.963551608,
@@ -50,7 +50,7 @@ public class SignalAnalysisTest
     public void Test_SST()
     {
         Assert.AreEqual(9.092723081398477, Statistics.Descriptive.SST(dataRandom), 1e-15);
-        Assert.AreEqual(9.092723081398475, Statistics.Descriptive.SSTParallel(dataRandom), 1e-15);
+        Assert.AreEqual(9.09272308139847, Statistics.Descriptive.SSTParallel(dataRandom), 1e-14);
 
         Assert.AreEqual(2560710.11, Statistics.Descriptive.SST(dataPrimes), 1e-2);
         Assert.AreEqual(2560710.11, Statistics.Descriptive.SSTParallel(dataPrimes), 1e-2);
