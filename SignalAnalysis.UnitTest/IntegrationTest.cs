8fc525a5e032a640bfb4c67eec7e603bf0b062d1
arthurits <simply_alfy@hotmail.com>
2024-02-24 13:04:00 +0100

    Add mid-point rule integration tests

--- SignalAnalysis.UnitTest/IntegrationTest.cs
+++ SignalAnalysis.UnitTest/IntegrationTest.cs
@@ -80,4 +80,28 @@ public class IntegrationTest
         Assert.AreEqual(0, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x) + 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.RightPointRule, 0, 1, 100, false), 1e-15);
         Assert.AreEqual(0.63641031907547874, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x) + 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.RightPointRule, 0, 1, 100, true), 1e-15);
     }
+
+    [TestMethod]
+    public void Test_Integration_MidPoint()
+    {
+        // Test data array input
+        Assert.AreEqual(0, Integration.Integrate(sin1Hz, IntegrationMethod.MidPointRule, sin1Hz.GetLowerBound(0), sin1Hz.GetUpperBound(0), 100, false, false), 1e-10);
+        Assert.AreEqual(0.63641031908, Integration.Integrate(sin1Hz, IntegrationMethod.MidPointRule, sin1Hz.GetLowerBound(0), sin1Hz.GetUpperBound(0), 100, true, false), 1e-10);
+
+        Assert.AreEqual(0, Integration.Integrate(sin2Hz, IntegrationMethod.MidPointRule, sin2Hz.GetLowerBound(0), sin2Hz.GetUpperBound(0), 100, false, false), 1e-10);
+        Assert.AreEqual(0.31789089679, Integration.Integrate(sin2Hz, IntegrationMethod.MidPointRule, sin2Hz.GetLowerBound(0), sin2Hz.GetUpperBound(0), 100, true, false), 1e-10);
+
+        Assert.AreEqual(0, Integration.Integrate(sinSum, IntegrationMethod.MidPointRule, sinSum.GetLowerBound(0), sinSum.GetUpperBound(0), 100, false, false), 1e-10);
+        Assert.AreEqual(0.63641031904, Integration.Integrate(sinSum, IntegrationMethod.MidPointRule, sinSum.GetLowerBound(0), sinSum.GetUpperBound(0), 100, true, false), 1e-10);
+
+        // Test function input
+        Assert.AreEqual(0, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x), IntegrationMethod.MidPointRule, 0, 1, 100, false), 1e-15);
+        Assert.AreEqual(0.636724504181952, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x), IntegrationMethod.MidPointRule, 0, 1, 100, true), 1e-15);
+
+        Assert.AreEqual(0, Integration.Integrate((x) => 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.MidPointRule, 0, 1, 100, false), 1e-15);
+        Assert.AreEqual(0.31851942219817281, Integration.Integrate((x) => 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.MidPointRule, 0, 1, 100, true), 1e-15);
+
+        Assert.AreEqual(0, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x) + 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.MidPointRule, 0, 1, 100, false), 1e-15);
+        Assert.AreEqual(0.63672450418195237, Integration.Integrate((x) => Math.Sin(2 * Math.PI * 1 * x) + 0.5 * Math.Sin(2 * Math.PI * 2 * x), IntegrationMethod.MidPointRule, 0, 1, 100, true), 1e-15);
+    }
 }
