15d1c748e9b2f83f93fc70a62b5bddb51175a76c
—ins— <insht_cnct@hotmail.es>
2026-03-19 12:02:06 +0100

    Add GetService to App class

--- SignalAnalysis.WinUI/App.xaml.cs
+++ SignalAnalysis.WinUI/App.xaml.cs
@@ -12,6 +12,16 @@ public partial class App : Application
 {
     public IHost Host { get; }
 
+    public static T GetService<T>() where T : class
+    {
+        if ((App.Current as App)!.Host.Services.GetService(typeof(T)) is not T service)
+        {
+            throw new ArgumentException($"{typeof(T)} needs to be registered in ConfigureServices within App.xaml.cs.");
+        }
+
+        return service;
+    }
+
     private Window? _window;
 
     /// <summary>
