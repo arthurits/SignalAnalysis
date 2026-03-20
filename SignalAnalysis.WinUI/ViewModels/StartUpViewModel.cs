98984ef6804eef6da17ab2b641aa6a6d6fda1f4f
arthurits <63915486+arthurits@users.noreply.github.com>
2026-03-20 14:27:15 +0100

    Add basic implementation to StartUpViewModel

--- SignalAnalysis.WinUI/ViewModels/StartUpViewModel.cs
+++ SignalAnalysis.WinUI/ViewModels/StartUpViewModel.cs
@@ -1,14 +1,25 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using SignalAnalysis.Contracts.Services;
 
 namespace SignalAnalysis.ViewModels;
 
 public partial class StartUpViewModel: ObservableRecipient
 {
-    public StartUpViewModel()
+    private readonly ILocalizationService _localizationService;
+
+    public StartUpViewModel(ILocalizationService localizationService)
     {
+        // Subscribe to localization service events
+        _localizationService = localizationService;
+        _localizationService.LanguageChanged -= OnLanguageChanged;
+        _localizationService.LanguageChanged += OnLanguageChanged;
+
+        // Load string resources into binding variables for the UI
+        OnLanguageChanged(null, EventArgs.Empty);
     }
 
     public void Dispose()
     {
+        _localizationService.LanguageChanged -= OnLanguageChanged;
     }
 }
