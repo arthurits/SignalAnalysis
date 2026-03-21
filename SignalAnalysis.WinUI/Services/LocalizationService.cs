42f8491276ef11cdc9fb0aad8f0870d5242122d2
arthurits <63915486+arthurits@users.noreply.github.com>
2026-03-21 17:12:16 +0100

    Refactor language detection in LocalizationService
    Improve detection of available languages by distinguishing between MSIX-packaged and unpackaged scenarios. Added async and sync helpers to enumerate language resources from package folders or file system as appropriate. Changed default resource file name to "Shell" and now always add invariant culture if requested. Old logic is commented out in favor of new, more robust approach. Added necessary usings for file and reflection operations.

--- SignalAnalysis.WinUI/Services/LocalizationService.cs
+++ SignalAnalysis.WinUI/Services/LocalizationService.cs
@@ -1,8 +1,13 @@
 ﻿using Microsoft.Windows.ApplicationModel.Resources;
-using System.Globalization;
-
 using SignalAnalysis.Contracts.Services;
+using SignalAnalysis.Helpers;
 using SignalAnalysis.Models;
+using System.Globalization;
+using System.Reflection;
+using Windows.ApplicationModel;
+using Windows.Foundation.Collections;
+using Windows.Globalization;
+using Windows.Storage;
 
 namespace SignalAnalysis.Services;
 
@@ -103,14 +108,15 @@ public class LocalizationService : ILocalizationService
     /// <param name="targetCulture">The culture used to create the returned list</param>
     /// <returns>List containing all the languages used in the <paramref name="resourceFileName"/></returns>
     public IEnumerable<CultureOption> GetAvailableLanguages(
-        string resourceFileName = "Resources",
+        string resourceFileName = "Shell",
         bool invariantCulture = true,
         bool loopAllKeys = false,
         CultureInfo? targetCulture = null)
     {
         var languages = new List<CultureOption>();
-        var resourceManager = Windows.ApplicationModel.Resources.Core.ResourceManager.Current;
-        var resourceMap = resourceManager.MainResourceMap;
+        //var resourceManager = Windows.ApplicationModel.Resources.Core.ResourceManager.Current;
+        //var resourceMap = resourceManager.MainResourceMap;
+        var resourceMap = _resourceManager.MainResourceMap.TryGetSubtree("About");
         var uniqueLanguages = new HashSet<string>();
         
         // Use the current UI culture if the parameter is not set
@@ -119,87 +125,132 @@ public class LocalizationService : ILocalizationService
         //Thread.CurrentThread.CurrentCulture = targetCulture;
 
         // Inspect available resources in folder
-        var resourceKeys = resourceMap.Keys.Where(k => k.StartsWith(resourceFileName)).ToList();
+        //var resourceKeys = resourceMap.Keys.Where(k => k.StartsWith(resourceFileName)).ToList();
+        var resourceKeys = new List<string>();
+        for (uint i = 0; i < resourceMap.ResourceCount; i++)
+        {
+            var kvp = resourceMap.GetValueByIndex(i); // KeyValuePair<TKey, TValue>
+            if (kvp.Key.StartsWith(resourceFileName, StringComparison.OrdinalIgnoreCase))
+            {
+                resourceKeys.Add(kvp.Key);
+            }
+        }
+
+        for (uint i = 0; i < resourceMap.ResourceCount; i++)
+        { 
+            var kvp = resourceMap.GetValueByIndex(i);
+            var named = resourceMap.TryGetValue(kvp.Key);
+            foreach (var candidate in named.QualifierValues)
+            {
+                    if (string.Equals(candidate.Key, "Language", StringComparison.OrdinalIgnoreCase))
+                    {
+                        var tag = candidate.Value; // p.ej. "es-ES"
+                                                   // normalizar y añadir a langs
+                    var cultureOption = createCultureOption(tag);
+                    if (cultureOption is not null)
+                    {
+                        languages.Add(cultureOption);
+                    }
+                }
+            }
+        }
+
+        if (RuntimeHelper.IsMSIX)
+        {
+            // Si la app se ejecuta como MSIX, se pueden obtener los idiomas disponibles inspeccionando las carpetas de recursos
+            var test = GetLanguagesFromPackageAsync();
+            //result.AddRange(languages);
+        }
+        else
+        {
+            // Si no es MSIX, se puede usar el método tradicional (aunque no siempre es fiable)
+            var test = GetAvailableLanguagesUnpackaged();
+            //result.AddRange(languages);
+        }
+        
 
         if (invariantCulture)
         {
-            var culture = CultureInfo.InvariantCulture;
-            if (uniqueLanguages.Add(culture.Name))
-            {
-                languages.Add(new CultureOption
-                {
-                    DisplayName = culture.DisplayName,  // This will be "Invariant Language (Invariant Country)" and does not change with the UI culture
-                    LanguageTag = culture.Name
-                });
-            }
+            var cultureOption = createCultureOption(string.Empty);
+            if (cultureOption is not null) { languages.Add(cultureOption); }
+
+            //var culture = CultureInfo.InvariantCulture;
+            //if (uniqueLanguages.Add(culture.Name))
+            //{
+            //    languages.Add(new CultureOption
+            //    {
+            //        DisplayName = culture.DisplayName,  // This will be "Invariant Language (Invariant Country)" and does not change with the UI culture
+            //        LanguageTag = culture.Name
+            //    });
+            //}
         }
 
-        if (loopAllKeys)
-        {
-            // This option should be used when keys might contain differente languages or qualifiers.
-            foreach (var key in resourceKeys)
-            {
-                var qualifiers = resourceMap[key].Candidates
-                    .SelectMany(c => c.Qualifiers)
-                    .Where(q => q.QualifierName == "Language")
-                    .Select(q => CultureInfo.GetCultureInfo(q.QualifierValue).Name)   // Normalizes the name to CultureInfo format (especially regarding upper and lower case)
-                    .Distinct()
-                    .ToList();
+        //if (loopAllKeys)
+        //{
+        //    // This option should be used when keys might contain differente languages or qualifiers.
+        //    foreach (var key in resourceKeys)
+        //    {
+        //        var qualifiers = resourceMap[key].Candidates
+        //            .SelectMany(c => c.Qualifiers)
+        //            .Where(q => q.QualifierName == "Language")
+        //            .Select(q => CultureInfo.GetCultureInfo(q.QualifierValue).Name)   // Normalizes the name to CultureInfo format (especially regarding upper and lower case)
+        //            .Distinct()
+        //            .ToList();
 
-                foreach (var language in qualifiers)
-                {
-                    var cultureOption = createCultureOption(language);
-                    if (cultureOption is not null)
-                    {
-                        languages.Add(cultureOption);
-                    }
-                    //if (TryGetCultureInfo(language, out var culture))
-                    //{
-                    //    if (uniqueLanguages.Add(language))
-                    //    {
-                    //        languages.Add(new CultureOption
-                    //        {
-                    //            DisplayName = culture?.DisplayName ?? string.Empty,
-                    //            LanguageTag = language
-                    //        });
-                    //    }
-                    //}
-                }
-            }
-        }
-        else
-        {    // This is much faster than iterating through all keys, but it assumes that the first key is representative of all the available languages.
-            var firstKey = resourceKeys.FirstOrDefault();
-            if (!string.IsNullOrEmpty(firstKey))
-            {
-                var qualifiers = resourceMap[firstKey].Candidates
-                    .SelectMany(c => c.Qualifiers)
-                    .Where(q => q.QualifierName == "Language")
-                    .Select(q => CultureInfo.GetCultureInfo(q.QualifierValue).Name) // Normalizes the name to CultureInfo format (especially regarding upper and lower case)
-                    .Distinct()
-                    .ToList();
+        //        foreach (var language in qualifiers)
+        //        {
+        //            var cultureOption = createCultureOption(language);
+        //            if (cultureOption is not null)
+        //            {
+        //                languages.Add(cultureOption);
+        //            }
+        //            //if (TryGetCultureInfo(language, out var culture))
+        //            //{
+        //            //    if (uniqueLanguages.Add(language))
+        //            //    {
+        //            //        languages.Add(new CultureOption
+        //            //        {
+        //            //            DisplayName = culture?.DisplayName ?? string.Empty,
+        //            //            LanguageTag = language
+        //            //        });
+        //            //    }
+        //            //}
+        //        }
+        //    }
+        //}
+        //else
+        //{    // This is much faster than iterating through all keys, but it assumes that the first key is representative of all the available languages.
+        //    var firstKey = resourceKeys.FirstOrDefault();
+        //    if (!string.IsNullOrEmpty(firstKey))
+        //    {
+        //        var qualifiers = resourceMap[firstKey].Candidates
+        //            .SelectMany(c => c.Qualifiers)
+        //            .Where(q => q.QualifierName == "Language")
+        //            .Select(q => CultureInfo.GetCultureInfo(q.QualifierValue).Name) // Normalizes the name to CultureInfo format (especially regarding upper and lower case)
+        //            .Distinct()
+        //            .ToList();
 
-                foreach (var language in qualifiers)
-                {
-                    var cultureOption = createCultureOption(language);
-                    if (cultureOption is not null)
-                    {
-                        languages.Add(cultureOption);
-                    }
-                    //if (TryGetCultureInfo(language, out var culture))
-                    //{
-                    //    if (uniqueLanguages.Add(language))
-                    //    {
-                    //        languages.Add(new CultureOption
-                    //        {
-                    //            DisplayName = culture?.DisplayName ?? string.Empty,
-                    //            LanguageTag = language
-                    //        });
-                    //    }
-                    //}
-                }
-            }
-        }
+        //        foreach (var language in qualifiers)
+        //        {
+        //            var cultureOption = createCultureOption(language);
+        //            if (cultureOption is not null)
+        //            {
+        //                languages.Add(cultureOption);
+        //            }
+        //            //if (TryGetCultureInfo(language, out var culture))
+        //            //{
+        //            //    if (uniqueLanguages.Add(language))
+        //            //    {
+        //            //        languages.Add(new CultureOption
+        //            //        {
+        //            //            DisplayName = culture?.DisplayName ?? string.Empty,
+        //            //            LanguageTag = language
+        //            //        });
+        //            //    }
+        //            //}
+        //        }
+        //    }
+        //}
 
         CultureOption? createCultureOption(string language)
         {
@@ -218,6 +269,139 @@ public class LocalizationService : ILocalizationService
         }
 
         return languages;
+    }
+
+    public async Task<IEnumerable<CultureOption>> GetLanguagesFromPackageAsync(string resourceFolderName = "Strings")
+    {
+        var result = new List<CultureOption>();
+        var unique = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        // Carpeta raíz del paquete
+        StorageFolder installed = Package.Current.InstalledLocation;
+
+        // Intentar abrir la carpeta "Strings" (puede variar según tu estructura)
+        StorageFolder? stringsFolder = null;
+        try
+        {
+            stringsFolder = await installed.GetFolderAsync(resourceFolderName);
+        }
+        catch
+        {
+            // No existe la carpeta Strings; devolver vacío o usar otra ruta
+            return result;
+        }
+
+        // Enumerar subcarpetas (cada una suele ser un language tag)
+        var subfolders = await stringsFolder.GetFoldersAsync();
+        foreach (var folder in subfolders)
+        {
+            var tag = folder.Name; // p.ej. "es-ES" o "en"
+            if (string.IsNullOrWhiteSpace(tag)) continue;
+
+            try
+            {
+                var ci = CultureInfo.GetCultureInfo(tag);
+                if (unique.Add(ci.Name))
+                {
+                    result.Add(new CultureOption
+                    {
+                        LanguageTag = ci.Name,
+                        DisplayName = ci.DisplayName
+                    });
+                }
+            }
+            catch (CultureNotFoundException)
+            {
+                // Si no es un tag de cultura válido, puedes ignorarlo o añadirlo crudo
+                if (unique.Add(tag))
+                {
+                    result.Add(new CultureOption
+                    {
+                        LanguageTag = tag,
+                        DisplayName = tag
+                    });
+                }
+            }
+        }
+
+        // Orden opcional
+        return result.OrderBy(r => r.DisplayName).ToList();
+    }
+
+    private IEnumerable<CultureOption> GetAvailableLanguagesUnpackaged(string resourceFolderName = "Strings")
+    {
+        var result = new List<CultureOption>();
+        var seen = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+
+        // 2) Fallback unpackaged: usar AppContext.BaseDirectory o la ubicación del ensamblado
+        string baseDir = AppContext.BaseDirectory;
+        if (string.IsNullOrEmpty(baseDir))
+        {
+            baseDir = Path.GetDirectoryName(Assembly.GetEntryAssembly()?.Location ?? Assembly.GetExecutingAssembly().Location) ?? Environment.CurrentDirectory;
+        }
+
+        // Buscar carpeta "Strings" bajo baseDir (estructura típica de .resw)
+        var stringsPath = Path.Combine(baseDir, resourceFolderName);
+        if (Directory.Exists(stringsPath))
+        {
+            foreach (var dir in Directory.GetDirectories(stringsPath))
+            {
+                var folderName = Path.GetFileName(dir);
+                AddCultureFromFolderName(folderName, result, seen);
+            }
+        }
+
+        // 3) Complemento: buscar carpetas de satellite assemblies (p.ej. "es-ES\MyApp.resources.dll")
+        // Recorremos subcarpetas directas de baseDir y comprobamos si contienen *.resources.dll
+        foreach (var dir in Directory.GetDirectories(baseDir))
+        {
+            var folderName = Path.GetFileName(dir);
+            try
+            {
+                // validar si folderName es un culture tag
+                var ci = CultureInfo.GetCultureInfo(folderName);
+                // comprobar si hay algún archivo *.resources.dll dentro
+                var hasResourcesDll = Directory.EnumerateFiles(dir, "*.resources.dll", SearchOption.TopDirectoryOnly).Any();
+                if (hasResourcesDll)
+                {
+                    AddCultureFromFolderName(folderName, result, seen);
+                }
+            }
+            catch (CultureNotFoundException)
+            {
+                // no es un culture tag, ignorar
+            }
+        }
+
+        // 4) Si no se ha encontrado nada, como último recurso devolver la cultura actual
+        if (result.Count == 0)
+        {
+            var cur = CultureInfo.CurrentUICulture;
+            result.Add(new CultureOption { LanguageTag = cur.Name, DisplayName = cur.DisplayName });
+        }
+
+        return result.OrderBy(r => r.DisplayName).ToList();
+    }
+
+    private void AddCultureFromFolderName(string folderName, List<CultureOption> list, HashSet<string> seen)
+    {
+        if (string.IsNullOrWhiteSpace(folderName)) return;
+        try
+        {
+            var ci = CultureInfo.GetCultureInfo(folderName);
+            if (seen.Add(ci.Name))
+            {
+                list.Add(new CultureOption { LanguageTag = ci.Name, DisplayName = ci.DisplayName });
+            }
+        }
+        catch (CultureNotFoundException)
+        {
+            // Si no es una cultura válida, puedes optar por ignorarla o añadirla cruda:
+            if (seen.Add(folderName))
+            {
+                list.Add(new CultureOption { LanguageTag = folderName, DisplayName = folderName });
+            }
+        }
     }
 
     // This method is commented out because the return values are not unique.
