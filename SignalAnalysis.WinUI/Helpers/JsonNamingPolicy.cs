7b8d8cc05e110bbd196ef6dbe12ec7f4c52b130e
arthurits <63915486+arthurits@users.noreply.github.com>
2026-03-29 16:17:55 +0200

    Create JsonNamingPolicy helper function

---
+++ SignalAnalysis.WinUI/Helpers/JsonNamingPolicy.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace SignalAnalysis.Helpers;
+
+internal sealed class EluxlNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name)) return name;
+        return char.ToLowerInvariant(name[0]) + name.Substring(1);
+    }
+}
