498d8314e9ac258f4dccc48dd9e29a80cfe2d1d4
arthurits <63915486+arthurits@users.noreply.github.com>
2026-03-29 13:54:41 +0200

    Create elux file DTO model

---
+++ SignalAnalysis.WinUI/Models/EluxDto.cs
@@ -0,0 +1,21 @@
+﻿namespace SignalAnalysis.Models;
+
+/// <summary>
+/// Represents a data transfer object (DTO) for data in an elux file, containing metadata and other details.
+/// </summary>
+/// <remarks>This class is used to serialize and deserialize elux data, including information about the
+/// document type, file version, and signal data.</remarks>
+internal class EluxlDto
+{
+    public string DocumentType { get; set; } = string.Empty;
+    public double FileVersion { get; set; }
+    public string StartingTime { get; set; } = string.Empty;
+    public string EndingTime { get; set; } = string.Empty;
+    public string Duration { get; set; } = string.Empty;
+    public int SensorNumber { get; set; }
+    public int SeriesPoints { get; set; }
+    public double SamplingFrequency { get; set; }
+    public List<List<string>> SeriesNames { get; set; } = [];
+    public List<List<double>> SeriesData { get; set; } = [];
+}
+
