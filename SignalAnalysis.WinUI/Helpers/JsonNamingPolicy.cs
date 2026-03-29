66176d7be2d1e9fb4ddd078db31f6ea869c8f76c
arthurits <63915486+arthurits@users.noreply.github.com>
2026-03-29 16:20:56 +0200

    Update json naming policy

--- SignalAnalysis.WinUI/Helpers/JsonNamingPolicy.cs
+++ SignalAnalysis.WinUI/Helpers/JsonNamingPolicy.cs
@@ -7,6 +7,14 @@ internal sealed class EluxlNamingPolicy : JsonNamingPolicy
     public override string ConvertName(string name)
     {
         if (string.IsNullOrEmpty(name)) return name;
-        return char.ToLowerInvariant(name[0]) + name.Substring(1);
+        return char.ToLowerInvariant(name[0]) + name[1..];
     }
 }
+
+internal sealed class SignalNamingPolicy : JsonNamingPolicy
+{
+    public override string ConvertName(string name)
+    {
+        if (string.IsNullOrEmpty(name)) return name;
+        return char.ToLowerInvariant(name[0]) + name[1..];
+    }
