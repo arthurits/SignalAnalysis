2ebfe485fb9203ff125905b682072ff7737b279d
arthurits <63915486+arthurits@users.noreply.github.com>
2026-04-04 12:11:44 +0200

    Create the ScatterPlotView xaml code

---
+++ SignalAnalysis.WinUI/Controls/ScatterPlotView.xaml.cs
@@ -0,0 +1,15 @@
+using Microsoft.UI.Xaml.Controls;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SignalAnalysis.Controls
+{
+    public sealed partial class ScatterPlotView : UserControl
+    {
+        public ScatterPlotView()
+        {
+            InitializeComponent();
+        }
+    }
+}
